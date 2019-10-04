aabe086cf84353fb56617815293cf1ca602cb435
Alex Rook <alex.rook@gmail.com>
2019-10-04 13:00:16 -0600

    refactro drop cols and datetime

--- alex_cross/clean.py
+++ alex_cross/clean.py
@@ -11,7 +11,6 @@ def clean_it(data):
     df = pd.read_csv(data)
     
     df = pd.get_dummies(df, columns=['city', 'phone', 'luxury_car_user'])
-    df.drop(['luxury_car_user_False', 'city_Winterfell'], axis = 1, inplace=True)
     df['last_trip_date'] = pd.to_datetime(df['last_trip_date'])
     df['signup_date'] = pd.to_datetime(df['signup_date'])
     df['month'] = df['last_trip_date'].map(lambda x: x.month)
@@ -20,6 +19,32 @@ def clean_it(data):
     df['avg_rating_by_driver']= imp.fit_transform(df[['avg_rating_by_driver']]).ravel()
     df['avg_rating_of_driver']= imp.fit_transform(df[['avg_rating_of_driver']]).ravel()
     return df
+
+def drop_cols(df, cols_to_drop):
+    '''
+    inputs: data frame
+            list of columns to drop
+
+    returns:
+            dataframe with dropped columns
+    '''
+    for col in cols_to_drop:
+        df.drop([col], axis=1, inplace=True)
+    return df
+
+
+def col_to_date(df, cols_to_convert):
+    '''
+    inputs: pandas data frame
+            list of columns to convert to datetime
+
+    returns:
+            dataframe with converted columns
+    '''
+    for col in cols_to_drop:
+        df.drop([col], axis=1, inplace=True)
+    return df
+
 
 def impute(df, columns):
     '''
